7e714250273b7b9a2fc401f6bd42e924906f51f6
Wallinson Rocha <wallinsonrochadev@gmail.com>
2024-03-20 13:27:03 -0300

    Testes iniciais com a criação da rota get

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+// Precisamos colocar a router aqui para ser encontrada
+const clientRoute = require('./routes/clientRoute');
 
 // Converter as requisições em json
 app.use(express.json());
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/', clientRoute);
 
 module.exports = app;
